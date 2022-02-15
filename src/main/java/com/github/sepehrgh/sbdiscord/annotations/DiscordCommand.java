8ff6c3eabfd4c34126910aec25a0f9883ec4bf54
sepehr-gh <sepehrgh01@gmail.com>
2022-02-15 17:32:39 +0330

    feat: add annotations

---
+++ src/main/java/com/github/sepehrgh/sbdiscord/annotations/DiscordCommand.java
@@ -0,0 +1,16 @@
+package com.github.sepehrgh.sbdiscord.annotations;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DiscordCommand {
+    String command() default "";
+    String description() default "";
+}
