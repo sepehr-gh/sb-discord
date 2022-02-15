1dd254fcdb7ba176bb4e9db2c8e04887542dbcc8
sepehr-gh <sepehrgh01@gmail.com>
2022-02-15 16:59:36 +0330

    feat: initialize springboot project

---
+++ src/main/java/com/github/sepehrgh/sbdiscord/SpringbootDiscordApplication.java
@@ -0,0 +1,13 @@
+package com.github.sepehrgh.sbdiscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sepehrgh/sbdiscord/SpringbootDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sepehrgh.sbdiscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
