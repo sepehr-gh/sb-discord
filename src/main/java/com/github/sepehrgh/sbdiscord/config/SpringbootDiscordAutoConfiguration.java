8a64580f7af3e22cab48937a608276052681ce4a
sepehr-gh <sepehrgh01@gmail.com>
2022-03-23 11:47:57 +0430

    fix: set JDA as bean

--- src/main/java/com/github/sepehrgh/sbdiscord/config/SpringbootDiscordAutoConfiguration.java
+++ src/main/java/com/github/sepehrgh/sbdiscord/config/SpringbootDiscordAutoConfiguration.java
@@ -1,14 +1,14 @@
 package com.github.sepehrgh.sbdiscord.config;
+
 import com.github.sepehrgh.sbdiscord.config.properties.SBDiscordProperties;
-import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,14 +17,15 @@ import javax.security.auth.login.LoginException;
 
 @Configuration
 @ComponentScan("com.github.sepehrgh.sbdiscord")
-@ConditionalOnProperty(value = "spring.discord.enabled", havingValue = "true")
 @EnableConfigurationProperties(SBDiscordProperties.class)
+@Slf4j
 public class SpringbootDiscordAutoConfiguration {
 
     @ConditionalOnMissingBean(JDA.class)
-    @Builder
-    public JDA jda(@Value("${spring.discord.token}") String token, DiscordCommandListener discordCommandListener) throws LoginException {
-        net.dv8tion.jda.api.JDABuilder builder = net.dv8tion.jda.api.JDABuilder.createDefault(token);
+    @Bean
+    public JDA jda(SBDiscordProperties sbDiscordProperties, DiscordCommandListener discordCommandListener) throws LoginException {
+        log.info("Creating JDA bean ...");
+        net.dv8tion.jda.api.JDABuilder builder = net.dv8tion.jda.api.JDABuilder.createDefault(sbDiscordProperties.getToken());
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         builder.setBulkDeleteSplittingEnabled(false);
         builder.setCompression(Compression.NONE);
